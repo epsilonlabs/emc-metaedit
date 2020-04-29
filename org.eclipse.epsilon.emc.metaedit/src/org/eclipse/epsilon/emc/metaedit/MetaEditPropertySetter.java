f281d89c3a2167f05f494c6fab261d5c1223684a
Sina Madani <sinadoom@googlemail.com>
2020-04-29 15:11:17 +0100

    Compliance with new IPropertySetter

--- org.eclipse.epsilon.emc.metaedit/src/org/eclipse/epsilon/emc/metaedit/MetaEditPropertySetter.java
+++ org.eclipse.epsilon.emc.metaedit/src/org/eclipse/epsilon/emc/metaedit/MetaEditPropertySetter.java
@@ -1,12 +1,15 @@
 package org.eclipse.epsilon.emc.metaedit;
 
+import org.eclipse.epsilon.common.module.ModuleElement;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.eol.execute.context.IEolContext;
 import org.eclipse.epsilon.eol.execute.introspection.AbstractPropertySetter;
 
 public class MetaEditPropertySetter extends AbstractPropertySetter {
 
 	@Override
-	public void invoke(Object value) throws EolRuntimeException {
+	public void invoke(Object target, String property, Object value, ModuleElement ast, IEolContext context) throws EolRuntimeException {
+		// TODO Auto-generated method stub
 		
 	}
 
